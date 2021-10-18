b78df41b9a6abc889eaa9591e90426bbada56d2a
Ana Paula ribas krum <apaula-r@c2r1s5.42lisboa.com>
2021-10-18 22:00:19 +0100

    new memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
-
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-	//char *modsrc;
+	char *modsrc;
 	char *moddest;
 	modsrc = (char *)src;
 	moddest = (char *)dest;
-	if (modsrc && moddest < n)
+	if (!(modsrc) && !(moddest))
 		return (0);
-	else 
-		ft_memcpy(dest, src, n);
+	while (n != 0)
+	{
+		*moddest++ = *modsrc++;
+		n--;
+	}
 	return (dest);
 }
-/*
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	int i = 0;
+	char *modsrc;
+	char *moddest;
+	modsrc = (char *)src;
+	moddest = (char *)dest;
+	if (src < dest)
+	{
+		while (n != 0)
+		{
+			i = i + n;
+			moddest[i] = modsrc[i];
+			i--;
+			n--;
+		}
+	}
+	else 
+		return (ft_memcpy(dest, src, n));
+	return (dest);
+}
+
 int main(void)
 {
-	char source[256] = "3";
+	char source[256] = "33333";
 	char dest[256] = "ola";
-	printf("%s", ft_memcpy(dest, source, 3));
+	printf("%s", ft_memmove(dest, source, 5));
 }
-*/
+
+
